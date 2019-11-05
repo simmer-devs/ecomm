8fe1e9c18451a3ba9f3290eef95772e55dfe3279
Simmer <55367994+simmer-devs@users.noreply.github.com>
2019-11-04 19:30:32 -0700

    Add files via upload

---
+++ ecomm-front/src/core/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from './Layout'
+
+const Home = () => (
+    <Layout title = 'Home Page' description = 'Node React E-Commerce App'>
+        ...
+    </Layout>
+)
+
+export default Home;
